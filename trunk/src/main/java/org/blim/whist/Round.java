72fcbc8a342b447651021defd50c576e23a70e08
ljdenison <ljdenison@2eee9394-ac6a-11de-ba6e-69e41c8db662>
2009-10-05 12:04:28 +0000

    Fixed bug in HandStateServlet.

--- trunk/src/main/java/org/blim/whist/Round.java
+++ trunk/src/main/java/org/blim/whist/Round.java
@@ -1,16 +1,10 @@
 package org.blim.whist;
 
-import com.google.common.collect.Lists;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 
 @Entity
 public class Round {
-
-	private List<Card> deck = Card.createDeck();
 
 	private Long id;
 	
@@ -28,16 +22,4 @@ public class Round {
 	public Hand getHandThree() { return handThree; }
 	public Hand getHandFour() { return handFour; }
 
-	@Transient
-	public List<Card> getDeck() {
-		return deck;
-	}
-
-	public void resetData() {
-		handOne.clearCards();
-		handTwo.clearCards();
-		handThree.clearCards();
-		handFour.clearCards();
-		deck = Card.createDeck();
-	}
 }
