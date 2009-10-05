a7acdc5f28001b4a86b161e6468200c601cdfb65
ljdenison <ljdenison@2eee9394-ac6a-11de-ba6e-69e41c8db662>
2009-10-05 10:21:43 +0000

    Added Spring and Hibernate dependencies. Added plumbing for database code. web.xml Spring configuration commented.

--- src/main/java/org/blim/whist/GameData.java
+++ src/main/java/org/blim/whist/GameData.java
@@ -3,19 +3,32 @@ package org.blim.whist;
 import com.google.common.collect.Lists;
 import java.util.List;
 
-public class GameData {
-	List<Card> deck = Card.createDeck();
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 
+@Entity
+public class GameData {
+
+	private List<Card> deck = Card.createDeck();
+
+	private Long id;
+	
 	private Player playerOne = new Player();
 	private Player playerTwo = new Player();
 	private Player playerThree = new Player();
 	private Player playerFour = new Player();
+
+	@Id
+	public Long getId() { return this.id; }
+    public void setId(Long id) { this.id = id; }
 	
 	public Player playerOne() { return playerOne; }
 	public Player playerTwo() { return playerTwo; }
 	public Player playerThree() { return playerThree; }
 	public Player playerFour() { return playerFour; }
-	
+
+	@Transient
 	public List<Card> getDeck() {
 		return deck;
 	}
