ebc4d64fa103e66cb823bd3adac4ac428341254c
rob@blim.org <rob@blim.org@2eee9394-ac6a-11de-ba6e-69e41c8db662>
2009-10-06 22:03:59 +0000

    Updated datamodel to reflect new layout for Game, Round, Hands, Tricks

--- trunk/src/main/java/org/blim/whist/GameService.java
+++ trunk/src/main/java/org/blim/whist/GameService.java
@@ -5,20 +5,22 @@ import java.util.List;
 
 public class GameService {
 
-	public void dealRound(List<Card> deck, int numOfCards,Hand one, Hand two, Hand three, Hand four) {
+	public void dealRound(List<Card> deck, Round round) {
 		Collections.shuffle(deck);
 		
-		for (int i = 0; i < numOfCards; i++) {
-			one.addCard(deck.remove(0));
-			two.addCard(deck.remove(0));
-			three.addCard(deck.remove(0));
-			four.addCard(deck.remove(0));
+		List<Hand> hands = round.getHands();
+		int numberOfCards = round.getNumberOfCards();
+		
+		for (int i = 0; i < numberOfCards; i++) {
+			for (Hand hand : hands) {
+				hand.addCard(deck.remove(0));
+			}
 		}
 	}
 	
-	public void playCard(List<Card> player, Card cardToPlay, Trick trick) {
-		trick.addCard(cardToPlay);
-		player.remove(cardToPlay);
+	public void playCard(String name, List<Card> hand, Card cardToPlay, Trick trick) {
+		trick.addCard(name, cardToPlay);
+		hand.remove(cardToPlay);
 	}
 		
 }
