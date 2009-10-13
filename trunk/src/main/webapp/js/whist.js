703521c7623b1d63f6e1d9eace33e7b7b220d4d6
ljdenison <ljdenison@2eee9394-ac6a-11de-ba6e-69e41c8db662>
2009-10-13 21:52:30 +0000

    Refactored javascript code - not yet complete.

--- trunk/src/main/webapp/js/whist.js
+++ trunk/src/main/webapp/js/whist.js
@@ -1,281 +1,42 @@
-var pollInterval = 300;
-var gameState = new Object();
-gameState.previousRound = new Object();
-gameState.previousRound.bids = new Array();
-gameState.previousRound.scores = new Array();
-gameState.currentRound = new Object();
-gameState.currentRound.bids = new Array();
-gameState.currentRound.scores = new Array();
-gameState.currentRound.playersPlayed = new Array();
+var xmlHttp = new Array();
 
-var handXmlHttp;
-var handTimer;
-function handStateChange()
+var game = new Object();
+game.pollInterval = 500;
+
+var user = null;
+
+function onLoadEventHandler() 
 {
-    if (handXmlHttp.readyState==4)
-    {// 4 = "loaded"
-        if (handXmlHttp.status==200)
-        {// 200 = OK
-            try {
-                var hand = JSON.parse(handXmlHttp.responseText);
-            } catch (e) {
-                alert("An exception occurred in the script. Error name: " + e.name 
-                  + ". Error message: " + e.message + ". Response was " + handXmlHttp.responseText); 
-            }
-            
-            if (hand.length > 0) {
-                var imgHTML = "";
-                for (card in hand) {
-                    imgHTML = imgHTML + "<img src = \"images/" + hand[card] + ".png"\" alt=\"hand[card]\"/>"
-                }
-                document.getElementById("handDiv").innerHTML = imgHTML;
-                gameState.cardCount = hand.length;
-                stateChange();
-            } else {
-                handTimer = setTimeout(dataRefresh("hand", pollInterval));
-            }
-        }
-        else
-        {
-            document.getElementById("handDiv").innerHTML = "<p>" + handXmlHttp.status + ": Data is out of data. Updating, please wait...</p>";
-        }
-    }
+	xmlHttp['round'].call();
+	xmlHttp['hand'].call();
+	xmlHttp['trick'].call();
 }
 
-var trickXmlHttp;
-function trickStateChange()
+function gameChangeEventHandler()
 {
-    if (trickXmlHttp.readyState==4)
-    {// 4 = "loaded"
-        if (trickXmlHttp.status==200)
-        {// 200 = OK
-            try {
-                var trick = JSON.parse(trickXmlHttp.responseText);
-            } catch (e) {
-                alert("An exception occurred in the script. Error name: " + e.name 
-                  + ". Error message: " + e.message + ". Response was " + trickXmlHttp.responseText); 
-            }
+	var phase = gamePhase();
+	var actingPlayer = actingPlayer(phase);
 
-            // Add the images for the ones we have a value for
-            var imgHTML = "";
-            for (var i=0; i < trick.length; i++) {
-            	if (trick[i] != null) {
-                    var imgToUpdate = document.getElementById("player" + i);
-                    imgHTML = "images/" + trick[i] + ".png";
-	                imgToUpdate.src = imgHTML;
-                    imgToUpdate.style.visibility = "visible";
-                }
-            }
-
-            for (var i = 0; i < ${fn:length(game.players)}; i++) {
-                var imgToUpdate = document.getElementById("player" + i);
-            	if (!trick[i]) {
-		           	imgToUpdate.style.visibility = "hidden";
-        	        imgToUpdate.src = "";
-	            	gameState.currentRound.playersPlayed[i] = "";
-	            } else {
-	                imgToUpdate.src = "images/" + trick[i] + ".png";
-                    imgToUpdate.style.visibility = "visible";
-	            	gameState.currentRound.playersPlayed[i] = trick[i];
-	            }
-            }
-           	
-            if (trick.length == ${fn:length(game.players)} ||
-            	gameState.currentRound.playerToPlay == ${user}) {
-                stateChange();
-            } else {
-                trickTimer = setTimeout(dataRefresh("trick", pollInterval));
-            }
-        }
-        else
-        {
-            document.getElementById("trickDiv").innerHTML = "<p>" + trickXmlHttp.status + ": Data is out of data. Updating, please wait...</p>";
-        }
-    }
-}
-        
-var scoresXmlHttp;
-var scoresTimer;
-function scoresStateChange()
-{
-    if (scoresXmlHttp.readyState==4)
-    {// 4 = "loaded"
-        if (scoresXmlHttp.status==200)
-        {// 200 = OK
-            try {
-                var scores = JSON.parse(scoresXmlHttp.responseText);
-            } catch (e) {
-                alert("An exception occurred in the script. Error name: " + e.name 
-                  + ". Error message: " + e.message + ". Response was " + scoresXmlHttp.responseText); 
-            }
-
-            document.getElementById("scoresTestBaseString").innerHTML = scoresXmlHttp.responseText;
-            
-            for (i = 0; i < ${fn:length(game.players)}; i++) {
-            	if (!scores.currentRound.bids[i]) {
-	            	document.getElementById("scoresDiv").getElementById("player" + i + "Bid").innerHTML = "";
-	            	gameState.currentRound.bids[i] = "";
-	            } else {
-	            	document.getElementById("scoresDiv").getElementById("player" + i + "Bid").innerHTML = scores.currentRound.bids[i];
-	            	gameState.currentRound.bids[i] = scores.currentRound.bids[i];
-	            }
-            }
-           	
-            document.getElementById("scoresDiv").getElementById("trumps").innerHTML = scores.currentRound.trumps;
-            gameState.currentRound.trumps = scores.currentRound.trumps;
-            document.getElementById("scoresDiv").getElementById("playerToBid").innerHTML = ${game.players[scores.currentRound.playerToBid]};
-            gameState.currentRound.playerToBid = scores.currentRound.playerToBid;
-            document.getElementById("scoresDiv").getElementById("numberOfCards").innerHTML = scores.currentRound.numberOfCards;
-            gameState.currentRound.numberOfCards = scores.currentRound.numberOfCards;
-            document.getElementById("scoresDiv").getElementById("bidWinner").innerHTML = ${game.players[scores.currentRound.bidWinner]};
-            gameState.currentRound.bidWinner = scores.currentRound.bidWinner;
-            gameState.currentRound.playerTurn = scores.currentRound.playerTurn;
-            gameState.gameFinished = scores.gameFinished;
-            
-            
-            if ((numberOfBids != ${fn:length(game.players)} && playerToBid != ${user}) ||
-                (numberOfBids == ${fn:length(game.players)} && scores.currentRound.trumps == "" && scores.currentRound.bidWinner != ${user})) {
-                scoresTimer = setTimeout(dataRefresh("scores", pollInterval));
-            } else {
-            	stateChange();
-            }
-        }
-        else
-        {
-            document.getElementById("scoresDiv").innerHTML = "<p>" + scoresXmlHttp.status + ": Data is out of data. Updating, please wait...</p>";
-        }
-    }
-}
-
-var bidSetXmlHttp;
-function bidSetStateChange()
-{
-    if (bidSetXmlHttp.readyState==4)
-    {// 4 = "loaded"
-        if (bidSetXmlHttp.status==200)
-        {// 200 = OK
-            try {
-                var bidResponse = JSON.parse(bidSetXmlHttp.responseText);
-            } catch (e) {
-                alert("An exception occurred in the script. Error name: " + e.name 
-                  + ". Error message: " + e.message + ". Response was " + bidSetXmlHttp.responseText); 
-            }
-        }
-        else
-        {
-        }
-    }
-}
-
-var trumpsSetXmlHttp;
-function trumpsSetStateChange()
-{
-    if (trumpsSetXmlHttp.readyState==4)
-    {// 4 = "loaded"
-        if (trumpsSetXmlHttp.status==200)
-        {// 200 = OK
-            try {
-                var trumpsResponse = JSON.parse(trumpsSetXmlHttp.responseText);
-            } catch (e) {
-                alert("An exception occurred in the script. Error name: " + e.name 
-                  + ". Error message: " + e.message + ". Response was " + trumpsSetXmlHttp.responseText); 
-            }
-        }
-        else
-        {
-        }
-    }
-}
-
-var playCardXmlHttp;
-function playCardStateChange()
-{
-    if (playCardXmlHttp.readyState==4)
-    {// 4 = "loaded"
-        if (playCardXmlHttp.status==200)
-        {// 200 = OK
-            try {
-                var playCardResponse = JSON.parse(playCardXmlHttp.responseText);
-            } catch (e) {
-                alert("An exception occurred in the script. Error name: " + e.name 
-                  + ". Error message: " + e.message + ". Response was " + playCardXmlHttp.responseText); 
-            }
-        }
-        else
-        {
-        }
-    }
-}
-
-function showBidUI()
-{
-	var html = "";
-	<c:forEach var="bid" begin="0" end="gameState.currentRound.numberOfState">
-		html = html + "<a href=\"javascript:AJAXPost(bidSetStateChange, '<c:url value="/bid"/>', '{&quot;id&quot;:${game.id}, &quot;bid&quot;:${bid}}');\">${bid}</a>"
- 	</c:forEach>
-	document.getElementById("bidsDiv").innerHTML = html;
-}
-
-function showSetTrumpUI()
-{
-	var suits = new Array("SPADES", "HEARTS", "DIAMONDS", "CLUBS");
-	var html = "";
-	for (suit in suits) {
-		html = 	html + "<a href=\"javascript:AJAXPost(trumpsSetStateChange,
-											   		  '<c:url value="/set-trumps"/>', 
-											   		  '{&quot;id&quot;:${game.id}, 
-											   		  &quot;trumps&quot;:&quot;suit&quot;}');\">suit</a>";
-	}
-	document.getElementById("setTrumpsDiv").innerHTML = html;
-}
-
-function allowCardPlay()
-{
-	var cards = document.getElementById("handDiv").getElementByTagName("img");
+	updateUI(phase, actingPlayer);
 	
-	for (card in cards) {
-		card.onClick = "AJAXPost(playCardStateChange,
-								 '<c:url value="/play-card"/>', 
-								 '{&quot;id&quot;:${game.id}, 
-								 &quot;card&quot;:&quot;card.alt&quot;}');";
+	if (actingPlayer != user) {
+		switch (phase) {
+			case "bid":
+				setTimeout(xmlHttp['round'].call(), game.pollInterval); 
+				break;
+			case "trick":
+				setTimeout(xmlHttp['trick'].call(), game.pollInterval); 
+				break;
+		}
 	}
 }
 
-function stopCardPlay()
-{
-	var cards = document.getElementById("handDiv").getElementByTagName("img");
+function gamePhase() {
+	// TODO: refactor code below to determine game phase
+	// Inspect the game data to determine what phase of the game 
+	// we are currently in
 	
-	for (card in cards) {
-		card.onClick = "";
-	}
-}
-
-function dataRefresh(type)
-{
-    if (type == "hand") {
-        handXmlHttp = AJAXGet(handStateChange, '<c:url value="/hand?id=${param.id}"/>');
-    } else if (type == "trick") {
-        trickXmlHttp = AJAXGet(trickStateChange, '<c:url value="/trick?id=${param.id}"/>');
-    } else if (type == "scores") {
-        scoresXmlHttp = AJAXGet(scoresStateChange, '<c:url value="/score?id=$param.id}"/>');
-    }
-}
-
-function stateChange()
-{
-	// Cancel all polls
-	if (handTimer) { clearTimeout(handTimer); }
-	if (trickTimer) { clearTimeout(trickTimer); }
-	if (scoresTimer) { clearTimeout(scoresTimer); }
-
-	// See if the game has finished
-	if (gameState.finished) {
-		return;
-	}
-		
-    // Check if we have a hand
-    if (gameState.cardCount == 0) {
-        dataRefresh("hand");
+    if (game.card == 0) {
         return;
     }
     
@@ -314,4 +75,164 @@ function stateChange()
     	stopCardPlay();
     	dataRefresh("trick");
     }
+  
+  	// Bidding test  
+    if ((numberOfBids != ${fn:length(game.players)} && playerToBid != ${user}) ||
+     	(numberOfBids == ${fn:length(game.players)} && scores.currentRound.trumps == "" && scores.currentRound.bidWinner != ${user})) {
+    }
 }
+
+function actingPlayer(phase) {
+	// TODO: refactor code below to determine the currently acting player
+	// Returns true if the game is waiting on the current
+	// user to act, false otherwise
+	
+    if (output.length == ${fn:length(game.players)} ||
+    	gameState.currentRound.playerToPlay == ${user}) {
+    }
+}
+
+function updateUI(phase, actingPlayer) {
+	// TODO: refactor the code below to update the UI components
+	// Determine which UI components to display based on phase
+	// and actingPlayer.  
+	// Inspect the game data 'updated' flags to determine which
+	// UI components must be re-rendered.
+
+	
+	// Bid snippet
+		var html = "";
+	<c:forEach var="bid" begin="0" end="gameState.currentRound.numberOfState">
+		html = html + "<a href=\"javascript:AJAXPost(bidSetStateChange, '<c:url value="/bid"/>', '{&quot;id&quot;:${game.id}, &quot;bid&quot;:${bid}}');\">${bid}</a>"
+ 	</c:forEach>
+	document.getElementById("bidsDiv").innerHTML = html;
+	
+	
+	// Trumps snippet
+		var suits = new Array("SPADES", "HEARTS", "DIAMONDS", "CLUBS");
+	var html = "";
+	for (suit in suits) {
+		html = 	html + "<a href=\"javascript:AJAXPost(trumpsSetStateChange,
+											   		  '<c:url value="/set-trumps"/>', 
+											   		  '{&quot;id&quot;:${game.id}, 
+											   		  &quot;trumps&quot;:&quot;suit&quot;}');\">suit</a>";
+	}
+	document.getElementById("setTrumpsDiv").innerHTML = html;
+	
+	
+	// Play card snippet
+		var cards = document.getElementById("handDiv").getElementByTagName("img");
+	
+	for (card in cards) {
+		card.onClick = "AJAXPost(playCardStateChange,
+								 '<c:url value="/play-card"/>', 
+								 '{&quot;id&quot;:${game.id}, 
+								 &quot;card&quot;:&quot;card.alt&quot;}');";
+	}
+
+
+	// Disable card snippet
+		var cards = document.getElementById("handDiv").getElementByTagName("img");
+	
+	for (card in cards) {
+		card.onClick = "";
+	}
+	
+	// Round scores snippet
+	document.getElementById("scoresTestBaseString").innerHTML = output;
+            
+    for (i = 0; i < ${fn:length(game.players)}; i++) {
+    	if (!scores.currentRound.bids[i]) {
+	    	document.getElementById("scoresDiv").getElementById("player" + i + "Bid").innerHTML = "";
+	        gameState.currentRound.bids[i] = "";
+	    } else {
+			document.getElementById("scoresDiv").getElementById("player" + i + "Bid").innerHTML = scores.currentRound.bids[i];
+	        gameState.currentRound.bids[i] = scores.currentRound.bids[i];
+	    }
+    }
+           	
+    document.getElementById("scoresDiv").getElementById("trumps").innerHTML = scores.currentRound.trumps;
+    gameState.currentRound.trumps = scores.currentRound.trumps;
+    document.getElementById("scoresDiv").getElementById("playerToBid").innerHTML = ${game.players[scores.currentRound.playerToBid]};
+    gameState.currentRound.playerToBid = scores.currentRound.playerToBid;
+    document.getElementById("scoresDiv").getElementById("numberOfCards").innerHTML = scores.currentRound.numberOfCards;
+    gameState.currentRound.numberOfCards = scores.currentRound.numberOfCards;
+    document.getElementById("scoresDiv").getElementById("bidWinner").innerHTML = ${game.players[scores.currentRound.bidWinner]};
+    
+    // Hand snippet
+        	var imgHTML = "";
+        for (card in output) {
+        	imgHTML = imgHTML + "<img src = \"images/" + output[card] + ".png"\" alt=\"output[card]\"/>"
+        }
+        document.getElementById("handDiv").innerHTML = imgHTML;
+        
+    // Trick snippet
+    	// Add the images for the ones we have a value for
+    var imgHTML = "";
+    for (var i=0; i < output.length; i++) {
+    	if (output[i] != null) {
+        	var imgToUpdate = document.getElementById("player" + i);
+            imgHTML = "images/" + output[i] + ".png";
+	        imgToUpdate.src = imgHTML;
+            imgToUpdate.style.visibility = "visible";
+        }
+    }
+
+    for (var i = 0; i < ${fn:length(game.players)}; i++) {
+    	var imgToUpdate = document.getElementById("player" + i);
+      	if (!output[i]) {
+			imgToUpdate.style.visibility = "hidden";
+        	imgToUpdate.src = "";
+	        gameState.currentRound.playersPlayed[i] = "";
+	    } else {
+	    	imgToUpdate.src = "images/" + output[i] + ".png";
+            imgToUpdate.style.visibility = "visible";
+	        gameState.currentRound.playersPlayed[i] = output[i];
+	    }
+    }
+           	
+    
+}
+
+xmlHttp['round'] = new JSONCallback();
+xmlHttp['round'].callback = function(output) {
+	// TODO: refactor the code below to update the game state with round data
+	// Update game state based on JSON output
+    gameState.currentRound.bidWinner = scores.currentRound.bidWinner;
+    gameState.currentRound.playerTurn = scores.currentRound.playerTurn;
+    gameState.gameFinished = scores.gameFinished;
+    
+    gameChangeEventHandler();
+}
+
+xmlHttp['hand'] = new JSONCallback();
+xmlHttp['hand'].callback = function(output)
+{
+	// TODO: refactor the code below to update the game state with hand data
+	// Update game state based on JSON output
+    gameState.cardCount = output.length;
+
+    gameChangeEventHandler();
+}
+
+xmlHttp['trick'] = new JSONCallback();
+xmlHttp['trick'].callback = function(output)
+{
+	// TODO: refactor the code below to update the game state with trick data
+	// Update game state based on JSON output
+ 
+    gameChangeEventHandler();
+}
+        
+
+xmlHttp['bid'] = new JSONCallback();
+xmlHttp['bid'].callback = function(output) {}
+
+xmlHttp['trumps'] = new JSONCallback();
+xmlHttp['trumps'].callback = function(output) {}
+
+xmlHttp['playCard'] = new JSONCallback();
+xmlHttp['playCard'].callback = function(output) {}
+
+
+
--- trunk/src/main/webapp/js/xmlhttp.js
+++ trunk/src/main/webapp/js/xmlhttp.js
@@ -1,47 +1,72 @@
-function AJAXGet(externalFunc, url)
+var JSONCallCount = 0;
+
+function JSONCallback() {}
+
+JSONCallback.prototype.xmlHttp;
+JSONCallback.prototype.errorElement;
+JSONCallback.prototype.callback;
+JSONCallback.prototype.url;
+
+JSONCallback.prototype.init = function(url, errorElement) 
 {
-    var xmlhttp=null;
-    if (window.XMLHttpRequest)
-    {// code for all new browsers
-        xmlhttp=new XMLHttpRequest();
-    }
-    else if (window.ActiveXObject)
-    {// code for IE5 and IE6
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    
-    if (xmlhttp!=null)
-    {
-        xmlhttp.onreadystatechange=externalFunc;
-        xmlhttp.open("GET",url,true);
-        xmlhttp.send(null);
-        return xmlhttp;
-    }
-    else
-    {
-        alert("Your browser does not support XMLHTTP.");
-        return null;
+	if (typeof errorElement == 'undefined') {
+		errorElement = "error";
+	}
+	
+	this.errorElement = errorElement;
+	this.url = url;
+}
+
+JSONCallback.prototype.xmlHttpCallback = function (xmlHttp)
+{
+	if (xmlHttp.readyState == 4)
+    {// 4 = "loaded"
+        if (xmlHttp.status == 200)
+        {// 200 = OK
+            try {
+                var JSONOutput = JSON.parse(xmlHttp.responseText);
+            } catch (e) {
+                alert("An exception occurred in the script. Error name: " + e.name 
+                  + ". Error message: " + e.message + ". Response was " + this.xmlHttp.responseText); 
+            }
+
+			this.callback(JSONOutput);
+        }
+        else
+        {
+            document.getElementById(this.errorElement).innerHTML = "<p>" + xmlHttp.status + ": Data is out of data. Updating, please wait...</p>";
+        }
+        
+        JSONCallCount--;
     }
 }
 
-function AJAXPost(externalFunc, url, data)
+JSONCallback.prototype.call = function(data)
 {
-    var xmlhttp=null;
+	var method = "POST";
+	
+	if (typeof data == 'undefined') {
+		method = "GET";
+		data = null;
+	}
+		
+	JSONCallCount++;
+
     if (window.XMLHttpRequest)
     {// code for all new browsers
-        xmlhttp=new XMLHttpRequest();
+        this.xmlHttp = new XMLHttpRequest();
     }
     else if (window.ActiveXObject)
     {// code for IE5 and IE6
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        this.xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     
-    if (xmlhttp!=null)
+    if (this.xmlHttp != null)
     {
-        xmlhttp.onreadystatechange=externalFunc;
-        xmlhttp.open("POST",url,true);
-        xmlhttp.send(data);
-        return xmlhttp;
+        this.xmlHttp.onreadystatechange = this.xmlHttpCallback(this.xmlHttp);
+        this.xmlHttp.open(method, this.url, true);
+        this.xmlHttp.send(data);
+        return this.xmlHttp;
     }
     else
     {
