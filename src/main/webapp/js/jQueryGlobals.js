f229ec922b523f5b3a1ef0d945288d7e893b3726
rob <rob@blim.org>
2010-04-06 19:17:39 +0000

    Some missed previous changes

--- src/main/webapp/js/jQueryGlobals.js
+++ src/main/webapp/js/jQueryGlobals.js
@@ -1,3 +1,29 @@
+function gameStartCall() {
+	$.ajax({
+        url: 'gameStart', 
+        data: JSON.stringify({id:game.id, phase:-1}), 
+        success: gameStartCallback,
+        progressAction: "Checking for start of game"
+    });
+}
+
+function updateCall(phase) {
+    $.ajax({
+        url: 'update',
+        data: JSON.stringify({id:game.id, phase:phase}),
+        success: updateCallback,
+        progressAction: "Updating"
+    });
+}
+
+function gameListCall() {
+    $.ajax({
+        url: 'games',
+        success: gameListCallback,
+        progressAction: "Retrieving available games"
+    });
+}
+
 function whistAjaxErrorHandler(xhr, textStatus, exception) {
     if (textStatus == 'timeout') {
         this.tryCount++;
@@ -12,10 +38,21 @@ function whistAjaxErrorHandler(xhr, textStatus, exception) {
     }
 }
 
+$(document).ajaxSend(function(e, xhr, settings) {
+	$("#ajaxProgressCount").text(settings.tryCount);
+	$("#ajaxProgressAction").text(settings.progressAction);
+    $("#ajaxProgress").css('display', 'inline');
+});
+
+$(document).ajaxStop(function() {
+	$("#ajaxProgress").css('display', 'none');
+});
+
 $.ajaxSetup({
-    tryCount: 0,
-    retryLimit: 3,
+    tryCount: 1,
+    retryLimit: 4,
     error: whistAjaxErrorHandler,
     timeout: 10000,
-    dataType: 'json'
+    dataType: 'json',
+    type: 'POST'
 });
