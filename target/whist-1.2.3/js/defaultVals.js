b650575831ee098589561e10b8d970ceead9980d
robkatedenison <robkatedenison@gmail.com>
2011-06-20 21:48:38 +0000

    Removed stupid commit of target dir

--- target/whist-1.2.3/js/defaultVals.js
+++
@@ -1,9 +0,0 @@
-ajaxInitTimeout = 500;
-ajaxTimeout = 5000;
-
-gameListUpdateDelay = 4000;
-gameStartUpdateDelay = 4000;
-gameStateUpdateDelay = 1000;
-
-regulariseMessagesDelay = 1500;
-showCurrentTrickCardsDelay = 1000;
